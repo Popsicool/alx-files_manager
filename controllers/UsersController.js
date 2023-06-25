7a7e2a14379efb4c3537afcbcbcc826c7c62ab2d
Popsicool <akinolasamson1234@gmail.com>
2023-06-25 17:31:16 +0100

    task 3-4

---
+++ controllers/UsersController.js
@@ -0,0 +1,52 @@
+import sha1 from 'sha1'
+import dbClient from '../utils/db'
+import redisClient from '../utils/redis'
+import {ObjectID} from 'mongodb'
+
+class UsersController{
+    static async postNew(req, res){
+        const {email} = req.body
+        const {password} = req.body
+        if (!email){
+            res.status(400).json({error: 'Missing email'})
+            return
+        }
+        if (!password){
+            res.status(400).json({error: 'Missing password'})
+            return
+        }
+        const user = await dbClient.db.collection('users').findOne({email})
+        if (user){
+            res.status(400).json({error: 'Already exist'})
+            return
+        }
+        const passwordHash = sha1(password)
+        await dbClient.db.collection('users').insertOne({email: email, password: passwordHash})
+        .then((result) => res.json({"id":result.insertedId,email})
+        )
+    }
+
+    static async getMe(req, res){
+        const users = await dbClient.db.collection('users')
+        const auths = req.header('X-Token');
+        if (!auths){
+            res.status(401).json({"error":"Unauthorized"})
+            return
+        }
+        const id = await redisClient.get(`auth_${auths}`)
+        if (!id){
+            res.status(401).json({"error":"Unauthorized"})
+            return
+        }
+        const userId = new ObjectID(id);
+        const user = await users.findOne({_id: userId})
+        if (!user){
+            res.status(401).json({"error":"Unauthorized"})
+            return
+        }
+
+        res.status(200).json({'email':user.email, id})
+    }
+}
+
+module.exports = UsersController
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 import { Router } from "express";
 import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController'
+import AuthController from '../controllers/AuthController'
 
 const routes = Router()
 
 routes.get('/status', AppController.getStatus)
 routes.get('/stats', AppController.getStats)
+routes.post('/users', UsersController.postNew)
+routes.get('/connect', AuthController.getConnect)
+routes.get('/disconnect', AuthController.getDisconnect)
+routes.get('/users/me', UsersController.getMe)
 
 module.exports = routes
